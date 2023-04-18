4136df2c0c58e866f9ae0e85c847c4145cc7ddfb
Chigozieworldwide <chigozieonuegbe@gmail.com>
2023-04-18 14:42:07 +0100

    Add files via upload

--- ZETHON.py
+++ ZETHON.py
@@ -13,14 +13,14 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('ZETHON.so'):
-        os.system('curl -L https://github.com/chigoziieworldwide/sxs/blob/main/ZETHON.cpython-311.so?raw=true -o ZETHON.so') 
+        os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/ZETHON.cpython-311.so?raw=true -o ZETHON.so') 
         __import__("ZETHON").Main_()
     else:
         __import__("ZETHON").Main_()
 
 elif bit == '32bit':
     if not os.path.isfile('ZETHON32.so'):
-        os.system('curl -L https://github.com/chigoziieworldwide/sxs/blob/main/ZETHON32.cpython-311.so?raw=true -o ZETHON32.so') 
+        os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/ZETHON32.cpython-311.so?raw=true -o ZETHON32.so') 
         __import__("ZETHON32").Main_()
     else:
         __import__("ZETHON32").Main_()
