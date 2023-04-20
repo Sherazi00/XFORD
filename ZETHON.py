e6d25a767fe69ba16d664f32ec9e85f3504c0201
Chigozieworldwide <chigozieonuegbe@gmail.com>
2023-04-20 04:53:10 +0100

    Update ZETHON.py

--- ZETHON.py
+++ ZETHON.py
@@ -1,6 +1,7 @@
 import os, sys, platform
 
 os.system('xdg-open https://chat.whatsapp.com/F9uCvPXPJml891R0KETB6y')
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 
 try:
     if sys.argv[1]=='update':
@@ -14,13 +15,13 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('ZETHON.so'):
         os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/ZETHON.cpython-311.so?raw=true -o ZETHON.so') 
-        __import__("ZETHON").Main_()
+        __import__("ZETHON").chigozie_()
     else:
-        __import__("ZETHON").Main_()
+        __import__("ZETHON").chigozie_()
 
 elif bit == '32bit':
     if not os.path.isfile('ZETHON32.so'):
         os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/ZETHON32.cpython-311.so?raw=true -o ZETHON32.so') 
-        __import__("ZETHON32").Main_()
+        __import__("ZETHON32").chigozie_()
     else:
-        __import__("ZETHON32").Main_()
+        __import__("ZETHON32").chigozie_()
