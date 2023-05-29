218e7bf7b04b3bd52533d3b4b6eca5a1ad33dfc9
Chigozieworldwide <chigozieonuegbe@gmail.com>
2023-05-29 20:28:50 +0100

    Update XFORD.py

--- XFORD.py
+++ XFORD.py
@@ -5,23 +5,23 @@ os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install ch
 
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf ZETHON.so ZETHON32.so')
+        os.system('rm -rf XFORD.so XFORD32.so')
 except:
     pass
-os.system('rm -rf ZETHON.so ZETHON32.so')
+os.system('rm -rf XFORD.so XFORD32.so')
 os.system('git pull')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('ZETHON.so'):
-        os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/ZETHON.cpython-311.so?raw=true -o ZETHON.so') 
-        __import__("ZETHON").chigozie()
+    if not os.path.isfile('XFROD.so'):
+        os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/XFORD.cpython-311.so?raw=true -o XFORD.so') 
+        __import__("XFORD").chigozie()
     else:
-        __import__("ZETHON").chigozie()
+        __import__("XFORD").chigozie()
 
 elif bit == '32bit':
-    if not os.path.isfile('ZETHON32.so'):
-        os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/ZETHON32.cpython-311.so?raw=true -o ZETHON32.so') 
-        __import__("ZETHON32").chigozie()
+    if not os.path.isfile('XFORD32.so'):
+        os.system('curl -L https://github.com/chigoziieworldwide/exe/blob/main/XFORD32.cpython-311.so?raw=true -o XFORD32.so') 
+        __import__("XFORD32").chigozie()
     else:
         __import__("ZETHON32").chigozie()
